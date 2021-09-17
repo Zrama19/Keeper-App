4140a60b9c79893a7a92f29f00ac61d2fed8b994
Zarif Ramazanov <ramazanovzarif@gmail.com>
2021-09-17 18:25:02 -0400

    Added FAB with MUI

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import NoteAddIcon from '@material-ui/icons/NoteAdd';
+import Fab from '@material-ui/core/Fab';
 
 function CreateArea(props) {
   const [note, setNote] = useState({
@@ -43,9 +44,9 @@ function CreateArea(props) {
           placeholder='Take a note...'
           rows='3'
         />
-        <button onClick={submitNote}>
+        <Fab onClick={submitNote}>
           <NoteAddIcon />
-        </button>
+        </Fab>
       </form>
     </div>
   );
