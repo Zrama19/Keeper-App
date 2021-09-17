2c0dc7e4a2ed2e8f89d4c7aa6d508d1cb509993b
Zarif Ramazanov <ramazanovzarif@gmail.com>
2021-09-17 16:55:55 -0400

    Added material-ui icons

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import AddCircleOutlineIcon from '@material-ui/icons/Add';
 
 function CreateArea(props) {
   const [note, setNote] = useState({
@@ -28,7 +29,7 @@ function CreateArea(props) {
 
   return (
     <div>
-      <form>
+      <form className='create-note'>
         <input
           name='title'
           onChange={handleChange}
@@ -42,7 +43,9 @@ function CreateArea(props) {
           placeholder='Take a note...'
           rows='3'
         />
-        <button onClick={submitNote}>Add</button>
+        <button onClick={submitNote}>
+          <AddCircleOutlineIcon />
+        </button>
       </form>
     </div>
   );
